9532659518e7dbb3f9febc8aa58df40fbce82a81
samuelmohel <samuelmohel@gmail.com>
2022-07-04 09:26:55 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
+#include <unistd.h>
 
 /**
- * main - Entry point*
+ * main - Entry point
+ *
  * Return: Always 1 (Success)
  *
  */
 
 int main(void)
-
 {
 
-		write(STDOUT_FILENO, "and that piece of art is useful
+		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-							\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
+		write(2, str1, 59);
+		return (1);
 
 }
