5c31044a1102376ddda39bdf44d9c2c086e13f79
samuelmohel <samuelmohel@gmail.com>
2022-07-04 09:34:49 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  * main - Entry point
  *
  * Return: Always 1 (Success)
- *
  */
 
 int main(void)
@@ -13,6 +12,7 @@ int main(void)
 		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 		write(2, str1, 59);
+
 		return (1);
 
 }
