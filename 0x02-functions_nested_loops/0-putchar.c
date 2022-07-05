f34995aa0c9f6529d8988faeb8dde8ed98e3cda8
samuelmohel <samuelmohel@gmail.com>
2022-07-05 06:54:12 -0700

    My first loop commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "_putchar.h"
+
+/**
+ * main - Check description
+ * Description: it prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char word [9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
