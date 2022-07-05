6b09a198a0991db4b186edd92fcbad1d6704f9f8
samuelmohel <samuelmohel@gmail.com>
2022-07-05 11:17:27 -0700

    new commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * int add - Check main
- *
+ * add - Check Holberton
+ * @a: An integer a
+ * @b: An integer b
  * Return: Result to add a and b.
  */
 
 int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
