2bb47bba854f3a551fd32b33224695c9166df2c8
samuelmohel <samuelmohel@gmail.com>
2022-07-13 06:51:45 -0700

    my advance commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -29,7 +29,7 @@ int main(void)
 
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half))
+			if (password[index] >= (33 + diff_half1))
 			{
 				password[index] -= diff_half1;
 				break;
@@ -37,7 +37,7 @@ int main(void)
 		}
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + dif_half2))
+			if (password[index] >= (33 + diff_half2))
 			{
 				password[index] -= diff_half2;
 				break;
