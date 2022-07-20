3700bad0e073ecd5b1aaaef7300a238ec9691292
samuelmohel <samuelmohel@gmail.com>
2022-07-20 12:48:14 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,7 @@ int _sqrt(int n, int i);
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: number to calculate the natural square root
- * 
+ *
  * Return: the natural square root
  */
 int _sqrt_recursion(int n)
