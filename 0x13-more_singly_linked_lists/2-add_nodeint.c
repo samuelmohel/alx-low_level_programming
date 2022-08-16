32e08af5989d82b9d296f14ea7c0d0768e08c02c
samuelmohel <samuelmohel@gmail.com>
2022-08-16 06:23:58 -0700

    My new commit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodient - adds a new node at the beginning of a listint_t list.
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @head: pointer to the first element of the list.
  * @n: int to set in the new node.
  * Return: address of the new element, or NULL if it failed
