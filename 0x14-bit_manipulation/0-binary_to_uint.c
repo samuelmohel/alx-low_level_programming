71a8857f557ff395c1429deaddad56c65b8cbb54
samuelmohel <samuelmohel@gmail.com>
2022-08-18 21:39:12 -0700

    new fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,22 @@
  */
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i;
+	unsigned int num;
 
+	num = 0;
 	if (!b)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
 	}
-
-	return (ui);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
